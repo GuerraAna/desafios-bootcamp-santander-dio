a36e95aa59e96f5af6ef7fd94dd60e092ac35af5
GuerraAna <ana.guerrads@hotmail.com>
2021-07-17 13:11:30 -0300

    Feat[main]: Solucionando Desafios em Kotlin - Teste de Selecao Um Challenge

--- src/SolucionandoDesafiosEmKotlin/pratica/CorridaDeTartarugas.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/CorridaDeTartarugas.kt
@@ -1,2 +1,5 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+
+
+
--- src/SolucionandoDesafiosEmKotlin/pratica/TesteDeSelecaoUm.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/TesteDeSelecaoUm.kt
@@ -1,2 +1,25 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+fun main(args: Array<String>) {
+
+    val input = readLine()!!
+
+    val A = input.split(" ")[0].toInt()
+    val B = input.split(" ")[1].toInt()
+    val C = input.split(" ")[2].toInt()
+    val D = input.split(" ")[3].toInt()
+
+        if (
+            (B > C) &&
+            (D > A) &&
+            ((C.plus(D)) > (A.plus(B))) &&
+            (C > 0) &&
+            (D > 0) &&
+            (A % 2 == 0)
+        ) {
+        println("Valores aceitos")
+    } else {
+        println("Valores nao aceitos")
+    }
+
+}
