db8b629fcd6703d01d22a2716fbee2645d4505d6
GuerraAna <ana.guerrads@hotmail.com>
2021-07-17 00:57:19 -0300

    Feat[main]: Primeiros passos em Kotlin - Media Um Challenge

--- src/PrimeirosPassosEmKotlin/MediaUm.kt
+++ src/PrimeirosPassosEmKotlin/MediaUm.kt
@@ -1,2 +1,19 @@
 package PrimeirosPassosEmKotlin
 
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    val A = readLine()!!.toFloat()
+    val B = readLine()!!.toFloat()
+
+    val weightA = 3.5
+    val weightB = 7.5
+    val totalWeight = weightA.plus(weightB)
+
+    val m = (A.times(weightA)).plus((B.times(weightB)))/ totalWeight
+
+    val r = "%.5f".format(m)
+
+    println("MEDIA = ${r}")
+}
