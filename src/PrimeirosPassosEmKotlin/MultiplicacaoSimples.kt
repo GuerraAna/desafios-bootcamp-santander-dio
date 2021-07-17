8fd2162158821543119a45f3b0f40bb71281984f
GuerraAna <ana.guerrads@hotmail.com>
2021-07-17 01:09:11 -0300

    Feat[main]: Primeiros passos em Kotlin - Multiplicacao Simples Challenge

--- src/PrimeirosPassosEmKotlin/MultiplicacaoSimples.kt
+++ src/PrimeirosPassosEmKotlin/MultiplicacaoSimples.kt
@@ -1,2 +1,11 @@
 package PrimeirosPassosEmKotlin
 
+fun main(args: Array<String>) {
+
+    val A = readLine()!!.toInt()
+    val B = readLine()!!.toInt()
+
+    val PROD = A.times(B)
+
+    println("PROD = $PROD")
+}
