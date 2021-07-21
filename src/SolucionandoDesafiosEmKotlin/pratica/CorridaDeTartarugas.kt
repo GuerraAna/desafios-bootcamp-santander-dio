fe78d0072f587516ac3e82a2c3abc9c957c3b620
GuerraAna <ana.guerrads@hotmail.com>
2021-07-21 00:16:25 -0300

    Feat[main]: Solucionando Desafios em Kotlin - A corrida de Tartarugas Challenge

--- src/SolucionandoDesafiosEmKotlin/pratica/CorridaDeTartarugas.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/CorridaDeTartarugas.kt
@@ -1,5 +1,30 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+fun main() {
+    while (true) {
+        try {
+            readLine()!!.toInt()
+            val v = readLine()!!.split(" ").run { map { it.toInt() } }
+            val sorted = v.toTypedArray()
+            sorted.sort()
 
+            when (sorted.last()) {
+                in 0 .. 9 -> {
+                    println(1)
+                }
+                in 10 .. 19 -> {
+                    println(2)
+                }else -> {
+                println(3)
+            }
+            }
 
-
+        } catch (f: Exception) {
+            break
+        } catch (n: Exception) {
+            break
+        } catch (e: Exception) {
+            break
+        }
+    }
+}
