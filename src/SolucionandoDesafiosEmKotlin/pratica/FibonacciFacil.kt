32d633909f1e6da860e91ec19be069c97943f3da
GuerraAna <ana.guerrads@hotmail.com>
2021-07-21 00:12:11 -0300

    Feat[main]: Solucionando Desafios em Kotlin - Fibonacci Facil Challenge

--- src/SolucionandoDesafiosEmKotlin/pratica/FibonacciFacil.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/FibonacciFacil.kt
@@ -1,2 +1,29 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+fun main() {
+
+    val N = readLine()!!.toInt()
+    fibonacci(N)
+
+}
+
+val fibonacci:(Int) -> Unit = { numero ->
+
+    var A = 1
+    var B = 0
+    var soma: Int
+    val lista:MutableList<String> = mutableListOf("0")
+    lista.add(" ")
+
+    for (i in 0 until numero.minus(1)){
+
+        lista.add(A.toString())
+        lista.add(" ")
+
+        soma = A
+        A += B
+        B = soma
+
+    }
+    lista.forEach { numeros-> print(numeros) }
+}
