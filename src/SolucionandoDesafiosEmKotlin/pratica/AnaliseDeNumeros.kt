2da13d6848924c6765176f0daa0361f1475aee3d
GuerraAna <ana.guerrads@hotmail.com>
2021-07-21 00:15:36 -0300

    Feat[main]: Solucionando Desafios em Kotlin - Analise De Numeros Challenge

--- src/SolucionandoDesafiosEmKotlin/pratica/AnaliseDeNumeros.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/AnaliseDeNumeros.kt
@@ -1,2 +1,39 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+fun main() {
+    mostrarResultado()
+}
+
+fun mostrarResultado(){
+
+    val inteiros = mutableListOf<Int>()
+    val pares = mutableListOf<Int>()
+    val impares = mutableListOf<Int>()
+    val positivos = mutableListOf<Int>()
+    val negativos = mutableListOf<Int>()
+
+    for (i in 1..5) {
+        inteiros.add(readLine()!!.toInt())
+    }
+
+    inteiros.forEach { numero->
+
+        if (numero%2 == 0) {
+            pares.add(numero)
+        }
+        if (numero%2 != 0) {
+            impares.add(numero)
+        }
+        if (numero>0) {
+            positivos.add(numero)
+        }
+        if (numero<0) {
+            negativos.add(numero)
+        }
+
+    }
+    println("${pares.size} valor(es) par(es)")
+    println("${impares.size} valor(es) impar(es)")
+    println("${positivos.size} valor(es) positivo(s)")
+    println("${negativos.size} valor(es) negativo(s)")
+}
