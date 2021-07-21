fc501bbafca8004bb7879f5c05eb1b721eefae7f
GuerraAna <ana.guerrads@hotmail.com>
2021-07-21 00:16:02 -0300

    Feat[main]: Solucionando Desafios em Kotlin - Triangulo Challenge

--- src/SolucionandoDesafiosEmKotlin/pratica/Triangulo.kt
+++ src/SolucionandoDesafiosEmKotlin/pratica/Triangulo.kt
@@ -1,2 +1,15 @@
 package SolucionandoDesafiosEmKotlin.pratica
 
+fun main() {
+
+    fun Double.format(digits: Long) = "%.${digits}f".format(this).replace(',','.')
+
+    val (a, b, c) = readLine()!!.split(" ").map { it.toDouble() }
+
+    val p = (a+b+c).format(1)
+    val r = (((a+b) * c / 2 )).format(1)
+
+    if (a < (b.plus(c)) &&  b < (a.plus(c)) &&  c < (a.plus(b))) println("Perimetro = $p")
+    else println("Area = $r")
+
+}
